e8d6845e7477b518d301c04fc5f862b5aa5efcf2
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2019-11-25 12:00:45 +0100

    📝 Include an example script

---
+++ examples/fix-ocr-d-gt-page.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+tmp_fg=FIXED_$RANDOM
+
+ocrd_repair_inconsistencies -I OCR-D-GT-PAGE -O $tmp_fg
+
+for f in $tmp_fg/*; do
+  g="OCR-D-GT-PAGE/OCR-D-GT-PAGE_${f#$tmp_fg/$tmp_fg_}"
+  cp $f $g
+done
+
+ocrd workspace remove-group -rf $tmp_fg
+rmdir $tmp_fg

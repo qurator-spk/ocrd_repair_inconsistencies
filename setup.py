2a2c97feffed3ae4a80c5081f332e7c2f5e747cc
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2019-11-29 18:21:47 +0100

    setup: version + include long description

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='ocrd_repair_inconsistencies',
+    version='0.1.0',
     description='Repair glyph/word/line order inconsistencies',
-    #long_description=codecs.open('README.md', encoding='utf-8').read(),
+    long_description=codecs.open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     author='Mike Gerber',
     author_email='mike.gerber@sbb.spk-berlin.de',
     license='Apache License 2.0',
